2b0d759862787c8d976f0b4624e9f0db4de64edc
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-04-03 23:29:40 +0300

    Final commit

--- src/Client.java
+++ src/Client.java
@@ -7,10 +7,20 @@ import java.util.Scanner;
 
 class Client implements Runnable {
     Socket socket;
+    Scanner in;
+    PrintStream out;
+    ChatServer chatServer;
 
-    public Client(Socket socket){
+    public Client(Socket socket, ChatServer chatServer){
 
         this.socket = socket;
+        this.chatServer = chatServer;
+        // запускаем поток
+        new Thread(this).start();
+    }
+
+    public void receive (String massage){
+        out.println(massage);
     }
 
     public void run() {
@@ -20,15 +30,15 @@ class Client implements Runnable {
             OutputStream os = socket.getOutputStream();
 
             // создаем удобные средства ввода и вывода
-            Scanner in = new Scanner(is);
-            PrintStream out = new PrintStream(os);
+            in = new Scanner(is);
+            out = new PrintStream(os);
 
             // читаем из сети и пишем в сеть
-            out.println("Welcome to mountains!");
+            out.println("Welcome to chat!");
             String input = in.nextLine();
             while (!input.equals("bye")) {
-                out.println(input + "-" + input + "-" +
-                        input.substring(input.length() / 2) + "...");
+
+                chatServer.sendAll(input);
                 input = in.nextLine();
             }
             socket.close();
