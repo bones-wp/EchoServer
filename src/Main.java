665c5c64f657cc3c85ff99187e3b12cb4aed1d33
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-04-03 15:39:52 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,28 +8,14 @@ public class Main {
         ServerSocket server = new ServerSocket(1234);
         while(true) {
             System.out.println("Waiting...");
-
-            // ждем клиента
+            // ждем клиента из сети
             Socket socket = server.accept();
             System.out.println("Client connected!");
-
-            // получаем потоки ввода и вывода
-            InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();
-
-            // создаем удобные средства ввода и вывода
-            Scanner in = new Scanner(is);
-            PrintStream out = new PrintStream(os);
-
-            // читаем из сети и пишем в сеть
-            out.println("Welcome to mountains!");
-            String input = in.nextLine();
-            while (!input.equals("bye")){
-                out.println(input + "-" + input + "-" +
-                        input.substring(input.length() / 2) + "...");
-                input = in.nextLine();
-            }
-            socket.close();
+            // создаем клиента на своей стороне
+            Client client = new Client(socket);
+            // запускаем поток
+            Thread thread = new Thread(client);
+            thread.start();
         }
     }
 }
